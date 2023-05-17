6b6ff9edc761fd0cb371168af33bce5bb2b873a3
frestytania <frestytania@gmail.com>
2023-05-17 09:26:13 +0700

    setup base html

--- project_django/settings.py
+++ project_django/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'project_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS' : {
+                'options': '-c search_path=u_league'
+            },
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'E0pu2OVKbytnbRKqGIql',
+        'HOST': 'containers-us-west-43.railway.app',
+        'PORT': '6826',
     }
 }
 
