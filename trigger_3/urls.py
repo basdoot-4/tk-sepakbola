4c25cfeeea71ead784f8c25c138336b87909a8cc
Mamdit <varokah@gmail.com>
2023-05-25 23:32:31 +0700

    Link

--- trigger_3/urls.py
+++ trigger_3/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
-from example_app.views import *
-from .views import *
+from trigger_3.views import *
+
+app_name = 'trigger_3'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
     path('list_all_Pertandingan', list_all_Pertandingan, name='list_all_Pertandingan'),
     path('pembuatanPertandingan', pembuatanPertandingan, name='pembuatanPertandingan'),
     path('deletePertandingan', deletePertandingan, name='deletePertandingan'),
